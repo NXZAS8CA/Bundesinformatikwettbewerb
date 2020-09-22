65188b5b59f47a87dfdfcd6200a2704e80d53b47
NXZAS8CA <52660198+NXZAS8CA@users.noreply.github.com>
2020-09-22 22:47:56 +0200

    Added Tabellenarray for Storage. Wishes are callable thru Array.
    Does not work with big .txt-Files

--- src/Input.java
+++ src/Input.java
@@ -1,22 +1,41 @@
 import java.io.*;
+import java.util.Arrays;
 
 public class Input {
+
+
+    private static String[] Reihenarray;
+    private static int Reihelength;
+
+
+
     public Input(String s) {
 
     }
 
-    public static void main(String[] args) {
-       try{File file = new File("C:\\Users\\NXZAS8CA\\Documents\\Coding\\Test.txt");
+    public static void main(String[] args) throws IOException {
+
+        File file = new File("C:\\Users\\NXZAS8CA\\Documents\\Coding\\Test.txt");
         BufferedReader br = new BufferedReader(new FileReader(file));
-           String AnzahlSchueler = br.readLine();
-           int AnzahlGeschenke = Integer.parseInt(AnzahlSchueler);
-           System.out.println(AnzahlGeschenke);
-        String st;
-        while ((st = br.readLine()) != null)
-            System.out.println(st);
-       }
-        catch (IOException e) {
-           e.printStackTrace();
-       }
+        String AnzahlSchueler = br.readLine();
+        int AnzahlGeschenke = Integer.parseInt(AnzahlSchueler);
+        String[][] Tabellenarray = new String[AnzahlGeschenke][3];
+
+
+        for(int i = 1; i <= AnzahlGeschenke; i ++){
+            String Reihe = br.readLine();
+            Reihenarray = Reihe.split("\\s+");
+            int length = Reihenarray.length;
+
+            for(int x = 1; x <length; x++){
+                Tabellenarray[i-1][x-1] =  Reihenarray[x];
+            }
+        }
+        System.out.println(Tabellenarray[1][0]);
+
+
+
+
+
     }
 }
