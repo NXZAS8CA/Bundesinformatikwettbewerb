6218e48fd10fc280718405cb4dba3b00ed69283d
NXZAS8CA <52660198+NXZAS8CA@users.noreply.github.com>
2020-09-25 18:09:25 +0200

    Added another class for sort-function

--- src/Input.java
+++ src/Input.java
@@ -1,24 +1,26 @@
 import java.io.*;
 
-public class Input {
+public class Input extends sort{
 
     private static String[] Reihenarray;
     private static int Reihelength;
+    public static String[][] Tabellenarray;
 
 
-
-    public Input(String s) {
+    public Input() {
 
     }
 
-    public static void main(String[] args) throws IOException {
+
+
+    public static void main() throws IOException {
 
         File file = new File("Test.txt");
         BufferedReader br = new BufferedReader(new FileReader(file));
 
         String AnzahlSchueler = br.readLine();
         int AnzahlGeschenke = Integer.parseInt(AnzahlSchueler);
-        String[][] Tabellenarray = new String[AnzahlGeschenke][3];
+        Tabellenarray = new String[AnzahlGeschenke][3];
 
 
         for(int i = 1; i <= AnzahlGeschenke; i ++){
@@ -26,11 +28,11 @@ public class Input {
             Reihenarray = Reihe.split("\\s+");
             int length = Reihenarray.length;
 
-            for(int x = 0; x <length; x++){K
+            for(int x = 0; x <length; x++){
                 Tabellenarray[i-1][x] =  Reihenarray[x];
             }
         }
-        System.out.println(Tabellenarray[0][0]);
+
 
     }
 }
--- src/sort.java
+++ src/sort.java
@@ -1,7 +1,15 @@
+import java.io.IOException;
+
 public class sort {
 
 
-}
-public static void Sortierung(){
+    public static void main(String[] args) throws IOException {
+        Input.main();//call main input function
+
+        System.out.println(Input.Tabellenarray[0][0]);
+
+    }
+
+
 
 }
