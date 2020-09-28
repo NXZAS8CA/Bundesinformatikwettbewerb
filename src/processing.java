478f1e0bb1d7a1b837c4756650a516504ab37263
JH.Home <joho.hoffmann@gmx.de>
2020-09-28 21:52:22 +0200

    Rall grade nix update

--- src/processing.java
+++ src/processing.java
@@ -3,10 +3,12 @@ import java.io.IOException;
 
 public class processing {
     private static long[] Endergebnis;
+    private static long[] Doppelte;
 
     public static void main(String[] args) throws IOException {
         sort.main();//call sort main function
         Endergebnis = new long[Input.AnzahlGeschenke];
+
         moveSingleNumbers(0);
     }
 
@@ -21,20 +23,28 @@ public class processing {
             }
         }
     }
-    public static void deleteNumbers(int input){
-        for(int i = 0; i <3; i++){
-            Input.Tabellenarray[input][i] = String.valueOf(0);
-        }
-    }
 
     public static void moveDoubleNumbers(int spalte){
         sort.countNumbers(spalte);
         for(int i = 0; i <sort.Geschenkezaehler.length;i++){
-            if(sort.Geschenkezaehler[i] == 2){}
+            if(sort.Geschenkezaehler[i] > 1){
+                Input.Tabellenarray[][];
+                for (int r = 0; r < Input.Tabellenarray.length; r++){
+                    if( Integer.parseInt(Input.Tabellenarray[r][0]) == i){
+                        deleteNumbers(r);
+                    }
+
+                }
+            }
 
 
         }
 
 
+    }
+    public static void deleteNumbers(int input){
+        for(int i = 0; i <3; i++){
+            Input.Tabellenarray[input][i] = String.valueOf(0);
+        }
     }
 }
