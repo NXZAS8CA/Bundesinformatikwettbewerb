f45ad505a19871c5807631fe7e0ba562b85a4444
NXZAS8CA <simon.krieger@mailbox.org>
2020-10-20 16:51:52 +0200

    added Output ability

---
+++ src/Output.java
@@ -0,0 +1,24 @@
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class Output {
+    public static void main(String[] args) throws IOException {
+        Processing.main();
+        File file = new File("Output.txt");
+        BufferedWriter bw = new BufferedWriter(new FileWriter(file));
+        bw.write("Schüleranzahl: " + Input.NumberGifts);
+        bw.newLine();
+        bw.write("Erfüllte erste Wünsche: ");
+        bw.newLine();
+        bw.write("Erfüllte zweite Wünsche:");
+        bw.newLine();
+        bw.write("Erfüllte dritte Wünsche:");
+        for(int i = 0; i < Input.NumberGifts; i++){
+            bw.
+
+        }
+        bw.close();
+    }
+}
