898922f0303c86dedbd290732ef463b6ad484d45
NXZAS8CA <simon.krieger@mailbox.org>
2020-10-28 13:52:09 +0100

    worked on recursion

--- src/Input.java
+++ src/Input.java
@@ -6,17 +6,18 @@ public class Input extends Extendprocessing {
     private static String[] Reihenarray;
     public static long[][] Tabellenarray;
     public static int NumberGifts;
-
+    public static String NumberStudents;
+    public static int AnzahlVerschiedenerWünsche = 3;
 
 
     public static void main() throws IOException {
 
-        File file = new File("Test.txt");
+        File file = new File("Test6.txt");
         BufferedReader br = new BufferedReader(new FileReader(file));
 
         String NumberStudents = br.readLine();
         NumberGifts = Integer.parseInt(NumberStudents);
-        Tabellenarray = new long[NumberGifts][3];
+        Tabellenarray = new long[NumberGifts][AnzahlVerschiedenerWünsche];
 
 
         for(int i = 1; i <= NumberGifts; i ++){
--- src/Processing.java
+++ src/Processing.java
@@ -12,6 +12,7 @@ public class Processing {
     public static int[] endergebnis;
     public static int[] wunsch;
     public static List<Integer> Vergeben;
+    public static int counter = 0;
 
     public static void main(String[] args) throws IOException {
         Extendprocessing.main();//call sort main function
@@ -28,8 +29,8 @@ public class Processing {
         moveSingleNumbers(0);
 
         moveMultipleNumbers(0, Wunscharray.get(0), 0);
-        moveMultipleNumbers(1, Wunscharray.get(0), 0);
-        moveMultipleNumbers(2, Wunscharray.get(0), 0);
+        //moveMultipleNumbers(1, Wunscharray.get(0), 0);
+        //moveMultipleNumbers(2, Wunscharray.get(0), 0);
         Extendprocessing.getIndexofMultipleNumbers(4, 0);
         Debug.printArraylits_Array(Wunscharray);
         Zwischenspeicher = Wunscharray.get(0);
@@ -60,6 +61,17 @@ public class Processing {
         }
         Wunscharray.add(wunsch);
         Endergebnisarray.add(endergebnis);
+
+
+
+        int[] first = Wunscharray.get(Wunscharray.size() - 1);
+
+        for (int k = 0; k < first.length; k++) {
+            if (first[k] == 1) {
+                counter++;
+            }
+        }
+
 
     }
 
@@ -94,18 +106,10 @@ public class Processing {
 
                     }
 
-                    int counter = 0;
-                    int[] last = Wunscharray.get(Wunscharray.size() - 1);
-
-                    for (int k = 0; k < last.length; k++) {
-                        if (last[k] == 1) {
-                            counter++;
-                        }
-                    }
-
-                    if (counterNeu > counter){
+                   if (counterNeu >= counter){
                         Wunscharray.add(arr2);
-
+                        counter = counterNeu;
+                        System.out.println(".");
                     }
 
 
